15601de1701f1a790d89c64ba7220e009708567a
P SIDHARTH <philkhanasidharth14@gmail.com>
2022-11-29 09:07:20 +0530

    Add files via upload

---
+++ ODD_EVEN.rb
@@ -0,0 +1,13 @@
+# Ruby program of integer odd? function
+  
+# Initializing the numbers 
+num1 = 19
+num2 = 2
+num3 = 28
+num4 = 13
+   
+# Prints true if number is odd
+puts num1.odd? 
+puts num2.odd?
+puts num3.odd? 
+puts num4.odd?
